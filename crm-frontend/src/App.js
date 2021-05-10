30f74f0db677566acbe4711aa6cea5f3e8f77426
lewist13 <lewistpro@gmail.com>
2021-05-10 00:49:38 -0400

    created entry page and rendered inside app

--- crm-frontend/src/App.js
+++ crm-frontend/src/App.js
@@ -1,7 +1,13 @@
+import React from "react";
 import "./styles/App.css";
+import { Entry } from "./pages/entry/Entry";
 
 function App() {
-  return <div className="App">hwllop</div>;
+  return (
+    <div className="App">
+      <Entry />
+    </div>
+  );
 }
 
 export default App;
