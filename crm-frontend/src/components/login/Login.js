d053fe22e1186d2106797d2818694c23e6bebe02
lewist13 <lewistpro@gmail.com>
2021-05-12 03:40:52 -0400

    built event handling functionality for login

--- crm-frontend/src/components/login/Login.js
+++ crm-frontend/src/components/login/Login.js
@@ -1,38 +1,66 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
-export const LoginForm = () => {
+export const LoginForm = ({
+  handleOnChange,
+  handleOnSubmit,
+  email,
+  password,
+}) => {
   return (
     <div>
       <Container>
         <Row>
           <Col>
-            <h1>Client Login</h1>
+            <h1 className="text-black">Client Login</h1>
             <hr />
-            <Form>
+            <Form autoComplete="off" onSubmit={handleOnSubmit}>
               <Form.Group>
-                <Form.Label>Email Address</Form.Label>
+                <Form.Label className="input-name m-1 pt-2">
+                  Email Address
+                </Form.Label>
                 <Form.Control
                   type="email"
                   name="email"
+                  value={email}
+                  onChange={handleOnChange}
                   placeholder="Enter Email"
                   required
                 />
               </Form.Group>
               <Form.Group>
-                <Form.Label>Password</Form.Label>
+                <Form.Label className="input-name m-1 pt-2">
+                  Password
+                </Form.Label>
                 <Form.Control
                   type="password"
                   name="password"
+                  value={password}
+                  onChange={handleOnChange}
                   placeholder="Password"
                   required
                 />
               </Form.Group>
-              <Button type="submit">Login</Button>
+              <Button className="login-button mt-2" type="submit">
+                Login
+              </Button>
             </Form>
+            <hr />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <a href="#!">Forgot Password?</a>
           </Col>
         </Row>
       </Container>
     </div>
   );
+};
+
+LoginForm.propTypes = {
+  handleOnChange: PropTypes.func.isRequired,
+  email: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired,
 };
--- crm-frontend/src/pages/entry/Entry.js
+++ crm-frontend/src/pages/entry/Entry.js
@@ -1,14 +1,44 @@
-import React from "react";
-import { Jumbotron } from "react-bootstrap";
+import React, { useState } from "react";
 import { LoginForm } from "../../components/login/Login";
 import "../../styles/Entry.css";
 
 export const Entry = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+
+    switch (name) {
+      case "email":
+        setEmail(value);
+        break;
+      case "password":
+        setPassword(value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    if (!email || !password) {
+      return alert("Fill up all the form!");
+    }
+    //TODO call api to submit form
+  };
+
   return (
     <div className="entry-page bg-info">
-      <Jumbotron>
-        <LoginForm />
-      </Jumbotron>
+      <div className="form-box h-20 p-5 bg-light border rounded-3 text-start">
+        <LoginForm
+          handleOnChange={handleOnChange}
+          handleOnSubmit={handleOnSubmit}
+          email={email}
+          password={password}
+        />
+      </div>
     </div>
   );
 };
