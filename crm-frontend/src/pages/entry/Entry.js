93a75f4677d18ee1c0d2d4b0186758d6c7387a3b
lewist13 <lewistpro@gmail.com>
2021-05-12 02:22:35 -0400

    rendered login form on entry page

--- crm-frontend/src/pages/entry/Entry.js
+++ crm-frontend/src/pages/entry/Entry.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Jumbotron } from "react-bootstrap";
+import { LoginForm } from "../../components/login/Login";
 import "../../styles/Entry.css";
 
 export const Entry = () => {
   return (
     <div className="entry-page bg-info">
-      <Jumbotron>fsafs</Jumbotron>
+      <Jumbotron>
+        <LoginForm />
+      </Jumbotron>
     </div>
   );
 };
